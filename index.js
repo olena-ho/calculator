ba8a1aa335851b26a61a9258b0abafe7afa7eba1
Olena Horovenko <elena.gorovenko@gmail.com>
2024-04-23 00:15:43 +0200

    styles and division done

---
+++ index.js
@@ -0,0 +1,53 @@
+const numberBtns = document.querySelectorAll('.btn--nmb');
+const screen = document.querySelector('.btn--screen');
+
+
+let storedNumbers = [];
+
+const makeUpNumber = (button) =>
+{
+  if (screen.textContent === '0') {
+    screen.textContent = '';
+  } 
+  const numberValue = button.textContent;
+  screen.textContent +=numberValue;
+
+}
+
+numberBtns.forEach(button => {
+  button.addEventListener('click', () => makeUpNumber(button));
+});
+
+const divideBtn = document.querySelector('.btn--divide');
+let mathAction = '';
+divideBtn.addEventListener('click', function() {
+  const individualNumber = parseFloat(screen.textContent);
+  storedNumbers.push(individualNumber);
+  screen.textContent = '0';
+  mathAction = '/';
+})
+
+const equalsBtn = document.querySelector('.btn--equals');
+
+equalsBtn.addEventListener('click', function() {
+  const individualNumber = parseFloat(screen.textContent);
+  storedNumbers.push(individualNumber);
+  screen.textContent = '0';
+  let result;
+  if (mathAction === '/') {
+    if (storedNumbers[1] === 0) {
+      result = 'Error: division by zero';
+    } else {
+      result = storedNumbers[0] / storedNumbers[1]
+    }
+  }
+  screen.textContent = result;
+  storedNumbers =[];
+})
+
+//clear
+const clearBtn = document.querySelector('.btn--clear');
+clearBtn.addEventListener('click', function() {
+  document.location.reload();
+})
+
