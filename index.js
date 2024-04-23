5299bd90b811622dfbeae5d24e88cf8c434514fc
Olena Horovenko <elena.gorovenko@gmail.com>
2024-04-23 12:17:39 +0200

    added delete function, and math for multiple numbers

--- index.js
+++ index.js
@@ -1,6 +1,92 @@
 const numberBtns = document.querySelectorAll('.btn--nmb');
 const screen = document.querySelector('.btn--screen');
+const progressBar = document.querySelector('.btn--progress');
+let progress = '';
 
+const makeUpNumber = (button) =>
+{
+  let currentScreenContent = screen.textContent;
+  if (currentScreenContent === '0') {
+    screen.textContent = '';
+  } else if (currentScreenContent[0] === '=') {
+    screen.textContent = '';
+    progressBar.textContent = '';
+    progress = '';
+  }
+  const numberValue = button.textContent;
+  screen.textContent +=numberValue;
+}
+
+numberBtns.forEach(button => {
+  button.addEventListener('click', () => makeUpNumber(button));
+});
+
+const divideBtn = document.querySelector('.btn--divide');
+
+divideBtn.addEventListener('click', function() {
+  progress += screen.textContent + ' / ';
+  progressBar.textContent = progress;
+
+  screen.textContent = '0';
+})
+
+const multiplyBtn = document.querySelector('.btn--multiply');
+multiplyBtn.addEventListener('click', function() {
+  progress += screen.textContent + ' * ';
+  progressBar.textContent = progress;
+
+  screen.textContent = '0';
+})
+const minusBtn = document.querySelector('.btn--minus');
+minusBtn.addEventListener('click', function() {
+  progress += screen.textContent + ' - ';
+  progressBar.textContent = progress;
+
+  screen.textContent = '0';
+})
+const plusBtn = document.querySelector('.btn--plus');
+plusBtn.addEventListener('click', function() {
+  progress += screen.textContent + ' + ';
+  progressBar.textContent = progress;
+
+  screen.textContent = '0';
+})
+
+const equalsBtn = document.querySelector('.btn--equals');
+
+equalsBtn.addEventListener('click', function() {
+  progress += screen.textContent;
+  progressBar.textContent = progress;
+
+  let result = eval(progress);
+  
+  screen.textContent = '= ' + result;
+})
+
+//clear
+const clearBtn = document.querySelector('.btn--clear');
+clearBtn.addEventListener('click', function() {
+  document.location.reload();
+})
+
+//back 
+
+const deleteOneDigit = document.querySelector('.btn--back');
+deleteOneDigit.addEventListener('click', function() {
+  let currentScreenText = screen.textContent;
+  if (currentScreenText.length > 1) {
+    screen.textContent = currentScreenText.slice(0, -1);
+  } else if (currentScreenText.length === 1) {
+    screen.textContent = '0';
+  }
+})
+
+
+//old
+/*
+const numberBtns = document.querySelectorAll('.btn--nmb');
+const screen = document.querySelector('.btn--screen');
+const progressBar = document.querySelector('.btn--progress');
 
 let storedNumbers = [];
 
@@ -11,7 +97,6 @@ const makeUpNumber = (button) =>
   } 
   const numberValue = button.textContent;
   screen.textContent +=numberValue;
-
 }
 
 numberBtns.forEach(button => {
@@ -21,28 +106,28 @@ numberBtns.forEach(button => {
 const divideBtn = document.querySelector('.btn--divide');
 let mathAction = '';
 divideBtn.addEventListener('click', function() {
-  const individualNumber = parseFloat(screen.textContent);
+  const individualNumber = Number(screen.textContent);
   storedNumbers.push(individualNumber);
   screen.textContent = '0';
   mathAction = '/';
 })
 const multiplyBtn = document.querySelector('.btn--multiply');
 multiplyBtn.addEventListener('click', function() {
-  const individualNumber = parseFloat(screen.textContent);
+  const individualNumber = Number(screen.textContent);
   storedNumbers.push(individualNumber);
   screen.textContent = '0';
   mathAction = '*';
 })
 const minusBtn = document.querySelector('.btn--minus');
 minusBtn.addEventListener('click', function() {
-  const individualNumber = parseFloat(screen.textContent);
+  const individualNumber = Number(screen.textContent);
   storedNumbers.push(individualNumber);
   screen.textContent = '0';
   mathAction = '-';
 })
 const plusBtn = document.querySelector('.btn--plus');
 plusBtn.addEventListener('click', function() {
-  const individualNumber = parseFloat(screen.textContent);
+  const individualNumber = Number(screen.textContent);
   storedNumbers.push(individualNumber);
   screen.textContent = '0';
   mathAction = '+';
@@ -51,7 +136,7 @@ plusBtn.addEventListener('click', function() {
 const equalsBtn = document.querySelector('.btn--equals');
 
 equalsBtn.addEventListener('click', function() {
-  const individualNumber = parseFloat(screen.textContent);
+  const individualNumber = Number(screen.textContent);
   storedNumbers.push(individualNumber);
   screen.textContent = '0';
   let result;
@@ -77,4 +162,4 @@ const clearBtn = document.querySelector('.btn--clear');
 clearBtn.addEventListener('click', function() {
   document.location.reload();
 })
-
+*/
